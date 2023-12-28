a32c33c7d463995bd2b8a26b7b3e5d2bfbacf45b
armichaud <email@alexandermichaud.tech>
2023-12-27 23:26:26 -0500

    Just gonna save progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::time;
 use std::{fs::File, io::{BufReader, BufRead}};
 
 use nalgebra::Vector2;
@@ -20,24 +21,30 @@ impl Hailstone {
         let p2 = Vector2::new(other.px as f64, other.py as f64);
         let v1 = Vector2::new(self.vx as f64, self.vy as f64);
         let v2 = Vector2::new(other.vx as f64, other.vy as f64);
+        // println!("p1: {}, p2: {}, v1: {}, v2: {}", p1, p2, v1, v2);
         let dp = p2 - p1;
         let dv = v1 - v2;
+        // println!("dp: {}, dv: {}", dp, dv);
         if dv == Vector2::zeros() {
             return false;
         }
-        let time_at_intersection = dp.dot(&dv) / dv.dot(&dv);
-        if time_at_intersection < 0.0 {
+        let time_at_intersection = dp.component_div(&dv);
+        let tx = time_at_intersection.x;
+        let ty = time_at_intersection.y;
+        // println!("T: {}", time_at_intersection);
+        if tx < 0.0 || ty < 0.0 {
             return false;
         }
-        let p1_at_intersection = self.get_position(time_at_intersection);
+        let p1_at_intersection = self.get_position(tx);
+        // let p2_at_intersection = other.get_position(ty);
+        // println!("p1: {}, p2: {}", p1_at_intersection, p2_at_intersection);
         let within_bound = p1_at_intersection.x >= lower && p1_at_intersection.x <= upper && p1_at_intersection.y >= lower && p1_at_intersection.y <= upper;
-        println!("p1x: {}, p1y: {}, p2x: {}, p2y: {}, p1_at_intersection: {:?}, {}", self.px, self.py, other.px, other.py, p1_at_intersection, within_bound);
         within_bound
     }
 
     fn get_position(&self, time: f64) -> Vector2<f64> {
-        let x = self.px as f64 + self.vx as f64 * time;
-        let y = self.py as f64 + self.vy as f64 * time;
+        let x = self.px as f64 + (self.vx as f64 * time);
+        let y = self.py as f64 + (self.vy as f64 * time);
         Vector2::new(x, y)
     }
 }
@@ -79,5 +86,5 @@ fn solution(file: &str, lower: f64, upper: f64) -> usize {
 
 fn main() {
     assert_eq!(solution("example.txt", 7.0, 27.0), 2);
-    assert_eq!(solution("input.txt", 200000000000000.0, 400000000000000.0), 3058);
+    // assert_eq!(solution("input.txt", 200000000000000.0, 400000000000000.0), 12045);
 }
